06e30b5b52d7d06b1b6beb54e4fd1a57e301cbe6
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-06-17 22:12:34 +0700

    initialize project

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="upper_header">
+        </div>
+        <div class="lower_header">
+            <div class="logo"></div>
+            <div class="navi_bar"><h1>thanh tìm kiếm và thanh điều hướng</h1></div>
+        </div>
+    </header>
+    <main>
+        <h1>main</h1>
+    </main>
+    <footer>
+        <h1>footer</h1>
+    </footer>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
+</body>
+</html>
