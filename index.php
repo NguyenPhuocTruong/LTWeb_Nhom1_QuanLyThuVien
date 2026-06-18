ae80d89821026bb65f1c1b77f50367f94177fe7f
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-06-18 19:03:58 +0700

    add some featers on header

--- index.php
+++ index.php
@@ -5,22 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
     <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/67ecaf9947.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
-        <div class="upper_header">
+    <div class="upper_header"></div>
+    <div class="lower_header">
+        <div class="logo"></div>
+        <div class="search">
+            <div class="search_bar">
+                <button class="search_button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <input type="search" placeholder="Tìm tên sách..." name="bookname">
+            </div>
+            <div class="login_container">
+                <div class="login">
+                    <i class="fa-solid fa-phone" style="font-size: 40px;"></i>
+                    <div><a href="" style="font-size: 19px; font-weight: bolder;">12345678</a><br><a href="" style="font-size: 15px;">Trợ giúp</a></div>
+                </div>
+                <div class="login">
+                    <i class="fa-solid fa-circle-user" style="font-size: 50px;"></i>
+                    <div><a href="" style="font-size: 19px; font-weight: bolder;">Đăng nhập</a><br><a href="" style="font-size: 15px;">Đăng ký</a></div>
+                </div>
+            </div>
         </div>
-        <div class="lower_header">
-            <div class="logo"></div>
-            <div class="navi_bar"><h1>thanh tìm kiếm và thanh điều hướng</h1></div>
+        <div class="catalogue">
+            <a href="">Sách Việt Nam <i class="fa-solid fa-angle-down"></i></a>
+            <a href="">Foreign Books <i class="fa-solid fa-angle-down"></i></a>
+            <a href="">Tác giả <i class="fa-solid fa-angle-down"></i></a>
+            <a href="">Thể loại <i class="fa-solid fa-angle-down"></i></a>
         </div>
-    </header>
+    </div>
     <main>
         <h1>main</h1>
     </main>
     <footer>
         <h1>footer</h1>
     </footer>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
+    <h1>he</h1>
+    <h1>lo</h1>
+    <h1>con</h1>
     <h1>he</h1>
     <h1>lo</h1>
     <h1>con</h1>
