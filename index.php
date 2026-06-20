ee26c591edb008c6d4546ee5a5154817af5235f3
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-06-20 14:08:24 +0700

    create file header.php

--- index.php
+++ index.php
@@ -8,60 +8,9 @@
     <script src="https://kit.fontawesome.com/67ecaf9947.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="upper_header"></div>
-    <div class="lower_header">
-        <a href=""><div class="logo"></div></a>
-        <div class="search">
-            <div class="search_bar">
-                <button class="search_button"><i class="fa-solid fa-magnifying-glass"></i></button>
-                <input type="search" placeholder="Tìm tên sách..." name="bookname">
-            </div>
-            <div class="login_container">
-                <div class="login">
-                    <i class="fa-solid fa-phone" style="font-size: 40px;"></i>
-                    <div><a href="" style="font-size: 19px; font-weight: bolder;">12345678</a><br><a href="" style="font-size: 15px;">Trợ giúp</a></div>
-                </div>
-                <div class="login">
-                    <i class="fa-solid fa-circle-user" style="font-size: 50px;"></i>
-                    <div><a href="" style="font-size: 19px; font-weight: bolder;">Đăng nhập</a><br><a href="" style="font-size: 15px;">Đăng ký</a></div>
-                </div>
-            </div>
-        </div>
-        <div class="catalogue">
-            <ul>
-                <li class="dropdown">
-                    <a href="">Sách Việt Nam <i class="fa-solid fa-angle-down"></i></a>
-                    <div class="dropdown_content">
-                        <a href="">Thể loại1</a>
-                        <a href="">Thể loại2</a>
-                        <a href="">Thể loại3</a>
-                        <a href="">Thể loại4</a>
-                        <a href="">Thể loại5</a>
-                    </div>
-                </li>
-                <li class="dropdown">
-                    <a href="">Foreign Books <i class="fa-solid fa-angle-down"></i></a>
-                    <div class="dropdown_content">
-                        <a href="">Thể loại1</a>
-                        <a href="">Thể loại2</a>
-                        <a href="">Thể loại3</a>
-                        <a href="">Thể loại4</a>
-                        <a href="">Thể loại5</a>
-                    </div>
-                </li>
-                <li class="dropdown">
-                    <a href="">Tác Giả <i class="fa-solid fa-angle-down"></i></a>
-                    <div class="dropdown_content">
-                        <a href="">Tác giả1</a>
-                        <a href="">Tác giả2</a>
-                        <a href="">Tác giả3</a>
-                        <a href="">Tác giả4</a>
-                        <a href="">Tác giả5</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <!-- include header -->
+    <?php include 'header.php' ?>
+    
     <main>
         <h1>main</h1>
     </main>
