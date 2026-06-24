f3aeff896b905ef0bb41535fe46194b2fd2546fe
chauthichcode <chaux500@gmail.com>
2026-06-24 22:26:25 +0700

    sidebar-admin

---
+++ admin/books.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books Admin</title>
+    <link rel="stylesheet" href="../assets/css/books.css">
+    <link rel="stylesheet" href="../assets/css/sidebar.css">
+</head>
+
+
+<body>
+    <div div class="container">
+        <div class="sidebar">
+            <?php include './sidebar.php'; ?>
+        </div>
+        <div class="content">Content</div>
+
+    </div>
+</body>
+
+</html>
---
+++ admin/sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+
+    <div class="logo">
+        📚 Library Admin
+    </div>
+
+    <a href="dashboard.php">🏠 Dashboard</a>
+    <a href="books.php">📖 Books</a>
+    <a href="users.php">👤 Users</a>
+    <a href="logout.php">🚪 Logout</a>
+
+</div>
