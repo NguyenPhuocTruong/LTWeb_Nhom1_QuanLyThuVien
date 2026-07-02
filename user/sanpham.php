166034ce3c2b8a59f6dc306683d2c667e68a6a5d
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-07-02 13:22:35 +0700

    update sanpham page

--- user/sanpham.php
+++ user/sanpham.php
@@ -15,71 +15,73 @@
         <div class="book_image">
             <div class="image_container">
                 <img src="../assets_user/book_images/bon_thoa_uoc.webp" alt="image">
+                <button>Mượn sách</button>
             </div>
         </div>
         <div class="book_info">
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
-            <h1>he</h1>
+            <div class="general_info">
+                <div class="title"><h1 id="book_name">Trí Tuệ Của Người Toltec - Bốn Thỏa Ước</h1><br><br></div>
+                <div class="info">
+                    <div>
+                        <p>Nhà cung cấp: <b>Saigon Books</b></p><br>
+                        <p id="nha_xuat_ban">Nhà xuất bản: <b>Dân trí</b></p>
+                    </div>
+                    <div>
+                        <p>Tác giả: <b>Janet Mills , don Miguel Ruiz</b></p><br>
+                        <p>Số lượng: <b>10</b></p>
+                    </div>
+                </div>
+            </div>
+            <div class="detail_info">
+                <h2>Thông tin chi tiết</h2>
+                <div><p class="info_name">Mã sách</p><p>12</p></div>
+                <div><p class="info_name">Tên nhà cung cấp</p><p>Saigon Books</p></div>
+                <div><p class="info_name">Tác giả</p><p>Janet Mills , don Miguel Ruiz</p></div>
+                <div><p class="info_name">NXB</p><p>Dân trí</p></div>
+                <div><p class="info_name">Năm XB</p><p>1990</p></div>
+            </div>
+            <div class="book_description">
+                <h2>Mô tả sản phẩm</h2><br>
+                BỐN THỎA ƯỚC
+Bí quyết sống tự do, bình an, hạnh phúc giữa thế giới bất định
+don Miguel Ruiz - Hậu duệ của người Toltec cổ xưa
+
+Hàng ngàn năm trước, người Toltec nổi tiếng khắp miền Nam Mexico là những “nhà thông thái”. Các nhà nhân chủng học nhắc đến người Toltec như một quốc gia, một dân tộc. Trên thực tế, người Toltec là một số những nhà khoa học và nghệ sĩ tập họp lại và tạo ra một xã hội riêng để cùng nghiên cứu, gìn giữ tri thức và cách thực hành tâm linh của những nền văn minh cổ đại. Họ là người hướng dẫn (nagual), là học trò của Teotihuacan – thành phố các kim tự tháp cổ đại nằm ở ngoại vi thành phố Mexico, được biết đến với tên gọi là “Nơi con người đắc đạo thành thần thánh”.
+
+Trải qua nhiều thiên niên kỷ, các vị nagual bị bắt buộc giữ bí mật toàn bộ những lời dạy thông thái cổ xưa này. Trong thời kỳ chinh phục của châu u, một số cá nhân thực hành tâm linh đã trở nên lạm quyền, hung hăng. Vì vậy, các nagual không còn cách nào khác là giấu kín, tránh cho những lời dạy này rơi vào tay những kẻ cố tình lợi dụng để làm những điều không đúng.
+
+May thay, tri thức bí truyền này của người Toltec vẫn được truyền từ đời này sang đời khác trong gia tộc của các nagual. Và dù đã được giữ bí mật hàng trăm năm, những lời tiên tri cổ xưa đã dự đoán rằng những lời dạy thông thái này rồi sẽ được mở ra cho người dân. Và giờ đây, Miguel Ruiz, một vị nagual của dòng tộc Hiệp sĩ Đại Bàng, đã được Bề Trên lựa chọn để chia sẻ cho đại chúng những lời dạy đầy quyền năng này của người Toltec.
+
+Giống như các đạo giáo bí truyền khác trên thế giới, lời dạy của người Toltec cũng bắt nguồn từ chân lý chung của vũ trụ. Mặc dù không được xem là đạo giáo, nhưng người hướng dẫn lời dạy Toltec đều được tri tôn như những bậc thầy. Và mặc dù có tinh thần tương tự như đạo giáo, lời dạy Toltec có thể xem như một phong cách sống lấy hạnh phúc và tình yêu thương làm kim chỉ nam.
+
+Bốn thỏa ước - Bộ quy tắc sống của người Toltec cổ xưa
+
+Trong Bốn thỏa ước, don Miguel Ruiz tiết lộ nguồn gốc của niềm tin tự giới hạn bản thân cướp đi niềm vui và tạo ra đau khổ không cần thiết. Dựa trên sự khôn ngoan của Toltec cổ xưa, Bốn thỏa ước đưa ra một quy tắc ứng xử mạnh mẽ có thể nhanh chóng biến cuộc sống của chúng ta sang một trải nghiệm mới về tự do, hạnh phúc thực sự và tình yêu.
+
+Theo tác giả, mọi việc một người làm đều dựa trên những thỏa thuận mà họ đã thực hiện với chính họ, với người khác, với Chúa, và với chính cuộc sống. Trong các thỏa ước này, mọi người có thể tự cho mình biết họ là ai, cư xử như thế nào, điều gì có thể và điều gì là không thể. Một số thỏa ước mà các cá nhân tạo ra có thể không gây ra vấn đề, nhưng có một số thỏa ước xuất phát từ tâm lý sợ hãi và có khả năng làm cạn kiệt năng lượng cảm xúc của một người cũng như làm giảm giá trị bản thân của chính họ.
+
+Ruiz tin rằng để tìm thấy niềm vui cá nhân, người ta phải thoát khỏi những khuôn mẫu do xã hội áp đặt và dựa trên nỗi sợ hãi có thể ảnh hưởng tiềm thức đến hành vi và suy nghĩ của cá nhân. Một trong những nội dung chính khác của cuốn sách cũng gợi ý rằng phần lớn đau khổ là do bản thân tạo ra và hầu hết thời gian, bạn có khả năng tự chuyển đổi những suy nghĩ tiêu cực mà bạn có thể có về các tình huống xảy ra trong cuộc sống của bạn. Tác giả xác định các nguồn gốc của bất hạnh trong cuộc sống và đề xuất bốn thỏa ước có lợi mà bạn có thể thực hiện với bản thân để cải thiện trạng thái hạnh phúc tổng thể của chính bạn. Bằng cách thực hiện một hiệp ước với bốn thỏa ước chính này, bạn có thể tác động đáng kể đến mức độ hạnh phúc mà chính bạn cảm thấy trong cuộc sống của mình, bất kể hoàn cảnh bên ngoài.
+
+Bốn thỏa ước là:
+
+1. Thỏa ước thứ nhất: Sử dụng ngôn từ tích cực
+
+Hãy nói lời chính trực. Nói đúng điều bạn nghĩ. Tránh dùng ngôn từ để chỉ trích bản thân hoặc đồn thổi về người khác. Sử dụng sức mạnh của sự thật và tình yêu trong ngôn từ.
+
+2. Thỏa thuận thứ hai: Không cho phép bản thân bị ảnh hưởng
+
+Bạn không phải chịu trách nhiệm cho bất cứ hành động nào của người khác. Những gì họ nói và làm chỉ phản ánh thực tế của chính họ, giấc mơ của chính họ. Khi bạn miễn nhiễm với quan điểm và hành động của người khác, bạn sẽ không trở thành nạn nhân của những đau khổ không cần thiết.
+
+3. Thỏa thuận thứ ba: Đừng bao giờ đưa ra giả định
+
+Hãy dũng cảm đặt ra những câu hỏi và bày tỏ điều mà bạn thực sự mong muốn. Hãy giao tiếp với người khác rõ ràng nhất có thể để tránh hiểu lầm, buồn rầu và khổ sở. Chỉ với một thỏa ước này thôi, bạn sẽ có thể hoàn toàn biến đổi cuộc sống của mình.
+
+4. Thỏa thuận thứ tư: Luôn cố gắng hết sức
+
+“Hết sức” là điều thay đổi tùy theo thời điểm. “Hết sức” khi bạn khỏe mạnh sẽ khác với “hết sức” khi bạn ốm đau. Trong bất kỳ hoàn cảnh nào, chỉ cần cố gắng hết sức, bạn sẽ tránh được việc tự phán xét, tự ngược đãi và hối hận.
+
+Hãy khám phá và cùng Bốn thỏa ước sống tự do, bình an và hạnh phúc giữa thế giới bất định này nhé!
+            </div>
         </div>
     </main>
     <?php include "../footer.php" ?>
