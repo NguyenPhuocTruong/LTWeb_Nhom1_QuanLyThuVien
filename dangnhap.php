d15e26a6ea80833831290faab27253ceb7257682
giahan-tran <giahan.tran0605@gmail.com>
2026-07-17 14:53:57 +0700

    Add files via upload

--- dangnhap.php
+++ dangnhap.php
@@ -4,7 +4,7 @@
     if($conn->connect_error){
         die("Connection failed: " .$conn->connect_error);
     }
-    
+
     $error = ''; 
 
     if(isset($_POST['login'])){
@@ -43,10 +43,97 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng nhập tài khoản</title>
+    <style>
+        .form-container{
+            position: fixed;
+            top: 0;
+            right: 0;
+            left:0;
+            bottom: 0;
+            display:flex;
+        }
+        .form-container__bg{
+            position: absolute;
+            width: 100%;
+            height:100%;
+            background: linear-gradient(0deg,#5AB9EA, #C1C8E4, #8860D0);    
+        }
+        .form-container__content{
+            width: 500px;
+            height: 500px;
+            background-color: #fff;
+            border-radius: 7px;
+            margin: auto;
+            position: relative;
+        }
+
+        h3{
+            font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            text-align: center;
+            margin-top: 60px;
+        }
+        .form-container__form{
+            padding: 0px 40px;
+        }
+        .form-container__row{
+            padding: 10px 0;
+        }
+        .form_label{
+            display: block;
+            margin-bottom: 10px;
+        
+        }
+        .form_input{
+            width: 100%;
+            height: 40px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        .form_policy{
+            display: flex;
+            align-items: flex-start;
+            gap:2px;
+            margin: 10px 0;
+        }
+
+        .policy-text{
+            text-align: center;
+        }
+        .policy-a{
+            text-decoration: none;
+        }
+        .btn{
+            color: #fff;
+            cursor: pointer;
+            transition: opacity 0.2s ease;
+        }
+        .btn:hover{
+            opacity: 0.8;
+        }
+        .btn-control{
+            width: 100%;
+            height: 40px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #8860D0;
+        }
+        .btn-social{
+            width: 100%;
+            height: 40px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            gap: 2px;
+        }
+        .form-login{
+            text-align: center;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <div class="form-container">
-        <div class="form-container__content log-in">
+        <div class="form-container__bg"></div>
+        <div class="form-container__content">
             <div class="form-container__header">
                 <h3>ĐĂNG NHẬP</h3>
             </div>
