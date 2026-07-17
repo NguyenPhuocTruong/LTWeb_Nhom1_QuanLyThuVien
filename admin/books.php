f456ad49a0fb8cf540cee954777dca06939b5895
chauthichcode <chaux500@gmail.com>
2026-07-17 18:25:48 +0700

    CRUD books

--- admin/books.php
+++ admin/books.php
@@ -5,7 +5,6 @@
     <title>Books</title>
 
     <link rel="stylesheet" href="../index.css">
-    <!-- <link rel="stylesheet" href="../assets_admin/css/reset.css"> -->
 
     <link rel="stylesheet" href="../assets_admin/css/sidebar.css">
     <link rel="stylesheet" href="../assets_admin/css/books.css">
@@ -16,7 +15,7 @@
 <body class="admin-body">
 
     <?php include "../header.php"; ?>
-
+    <?php include "../mysqlConnect.php"; ?>
     <div class="books-page">
 
         <div class="admin-layout">
@@ -27,42 +26,77 @@
 
                 <div class="books-header">
                     <h2>Manage Books</h2>
-                    <button class="btn-add">+ Add Book</button>
+                    <button class="btn-add"><a href="addBooks.php">+ Books</a></button>
                 </div>
 
                 <div class="books-table-box">
                     <table class="books-table">
                         <thead>
                             <tr>
-                                <th>Book</th>
-                                <th>ISBN</th>
-                                <th>Author</th>
-                                <th>Publisher</th>
-                                <th>Category</th>
-                                <th>Rack</th>
-                                <th>No of copy</th>
-                                <th>Status</th>
-                                <th>Updated</th>
-                                <th>Action</th>
+                                <th>Mã sách</th>
+                                <th>Tên sách</th>
+                                <th>Tác giả</th>
+                                <th>Nhà xuất bản</th>
+                                <th>Thể loại</th>
+                                <th>Quốc gia</th>
+                                <th>Nhà cung cấp</th>
+                                <th>Số lượng</th>
+                                <th>Năm XB</th>
+                                <th>Thao tác</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <td>The Joy of PHP</td>
-                                <td>12345</td>
-                                <td>Alan Forbes</td>
-                                <td>Plum Island</td>
-                                <td>Programming</td>
-                                <td>R2</td>
-                                <td>10</td>
-                                <td>Enable</td>
-                                <td>2026-06-24</td>
-                                <td>
-                                    <button class="btn-edit">Edit</button>
-                                    <button class="btn-delete">Delete</button>
-                                </td>
-                            </tr>
+
+                            <?php
+
+                            $sql = "SELECT * FROM sach";
+
+                            $result = mysqli_query($conn, $sql);
+                            $stt = 1;
+                            while ($row = mysqli_fetch_assoc($result)) {
+
+                            ?>
+
+                                <tr>
+
+                                    <td><?php echo $stt++; ?></td>
+
+                                    <td><?php echo $row['ten_sach']; ?></td>
+
+                                    <td><?php echo $row['tac_gia']; ?></td>
+
+                                    <td><?php echo $row['nha_xb']; ?></td>
+
+                                    <td><?php echo $row['the_loai']; ?></td>
+
+                                    <td><?php echo $row['quoc_gia']; ?></td>
+
+                                    <td><?php echo $row['nha_cung_cap']; ?></td>
+
+                                    <td><?php echo $row['so_luong']; ?></td>
+
+                                    <td><?php echo $row['nam_xb']; ?></td>
+
+                                    <td>
+
+                                        <button class="btn-edit"><a href="addBooks.php?id=<?php echo $row['ma_sach']; ?>"
+                                                class="btn-edit"
+                                                style="text-decoration: none; display: inline-block;">Edit</a></button>
+
+                                        <button class="btn-delete"><a
+                                                href="deleteBooks.php?id=<?php echo $row['ma_sach']; ?>" class="btn-delete"
+                                                style="text-decoration: none; display: inline-block;"
+                                                onclick="return confirm('Bạn có chắc chắn muốn xóa cuốn sách này không?');">Delete</a></button>
+
+                                    </td>
+
+                                </tr>
+
+                            <?php
+                            }
+                            ?>
+
                         </tbody>
                     </table>
                 </div>
