8c4609a9987925786bf1f579283b0f0945f9beb9
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-07-18 10:32:33 +0700

    merge user -> auth

--- auth/dangky.php
+++ auth/dangky.php
@@ -60,7 +60,7 @@ if (isset($_POST['dangki'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets_auth/form.css"> 
+    <link rel="stylesheet" href="../assets_auth/form.css"> 
     <script src="https://kit.fontawesome.com/67ecaf9947.js" crossorigin="anonymous"></script>
     <title>Đăng ký tài khoản</title>
     <!-- <style>
--- auth/dangnhap.php
+++ auth/dangnhap.php
@@ -42,7 +42,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets_auth/form.css">
+    <link rel="stylesheet" href="../assets_auth/form.css">
     <title>Đăng nhập tài khoản</title>
     <!-- <style>
         .form-container{
