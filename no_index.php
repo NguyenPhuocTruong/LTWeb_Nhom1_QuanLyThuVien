fe4afbc3218a0a585e077c7ff52d9c627eed4135
Nguyen Phuoc Truong <truongsteamgame@gmail.com>
2026-07-18 10:01:46 +0700

    Revert "merge auth -> user"
    This reverts commit 7218b8b5bff6b03d8f5138d94954e71dd9d8a2ef, reversing
    changes made to cfba152286983531d186f8cf3404940bc0f21082.

---
+++ no_index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/67ecaf9947.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/images/sky4.jpg">
+</head>
+<body>
+    <!-- include header file -->
+    <?php include 'header.php' ?>
+
+    <main>
+        <h1>MAIN</h1><br>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+        <h1>he</h1>
+        <h1>lo</h1>
+        <h1>con</h1>
+    </main>
+
+    <!-- include footer file -->
+    <?php include 'footer.php' ?>
+</body>
+</html>
